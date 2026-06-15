a497bae97b030e8c62675d2686b880981abbb648
werrik15-a11y <werrik15@gmail.com>
2026-06-15 14:21:19 +0300

    admin.js

--- js/admin.js
+++ js/admin.js
@@ -3,30 +3,24 @@ import { collection, query, orderBy, onSnapshot } from "https://www.gstatic.com/
 
 const q = query(collection(db, "players"), orderBy("score", "desc"));
 
-let prev = {};
-let chart;
-
 onSnapshot(q, (snap) => {
   const table = document.getElementById("table");
 
   let html = `
-    <tr><th>#</th><th>Имя</th><th>Баллы</th></tr>
+    <tr>
+      <th>#</th>
+      <th>Имя</th>
+      <th>Баллы</th>
+    </tr>
   `;
 
   let i = 1;
-  const players = [];
 
   snap.forEach(doc => {
     const d = doc.data();
-    players.push(d);
-
-    let cls = "";
-    if (d.score > (prev[d.name] || 0)) cls = "up";
-    if (d.score < (prev[d.name] || 0)) cls = "down";
-    prev[d.name] = d.score;
 
     html += `
-      <tr class="${cls}">
+      <tr>
         <td>${i}</td>
         <td>${d.name}</td>
         <td>${d.score}</td>
@@ -37,32 +31,4 @@ onSnapshot(q, (snap) => {
   });
 
   table.innerHTML = html;
-
-  renderChart(players);
 });
-
-function renderChart(players) {
-  const ctx = document.getElementById("chart");
-
-  const map = {};
-
-  players.forEach(p => {
-    map[p.score] = (map[p.score] || 0) + 1;
-  });
-
-  const labels = Object.keys(map);
-  const data = Object.values(map);
-
-  if (chart) chart.destroy();
-
-  chart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels,
-      datasets: [{
-        label: "Распределение баллов",
-        data
-      }]
-    }
-  });
-}
