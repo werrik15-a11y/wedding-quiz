45c2a36c809f1a971c4eb73c88e3f7b599031685
werrik15-a11y <werrik15@gmail.com>
2026-06-15 13:23:10 +0300

    admin.js

--- js/admin.js
+++ js/admin.js
@@ -14,16 +14,30 @@ const q = query(
 onSnapshot(q, (snap) => {
   const table = document.getElementById("table");
 
-  let html = "<tr><th>Имя</th><th>Баллы</th></tr>";
+  let html = `
+    <tr>
+      <th>#</th>
+      <th>Имя</th>
+      <th>Баллы</th>
+    </tr>
+  `;
 
-  let i = 0;
+  let i = 1;
 
   snap.forEach(doc => {
-    if (i < 10) {
-      const d = doc.data();
-      html += `<tr><td>${d.name}</td><td>${d.score}</td></tr>`;
-      i++;
+    const d = doc.data();
+
+    if (i <= 10) {
+      html += `
+        <tr>
+          <td>${i}</td>
+          <td>${d.name}</td>
+          <td>${d.score}</td>
+        </tr>
+      `;
     }
+
+    i++;
   });
 
   table.innerHTML = html;
