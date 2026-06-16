d123a478044a0d6647bf44a103c32147538b0f1c
werrik15-a11y <werrik15@gmail.com>
2026-06-16 11:40:15 +0300

    admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,81 @@
 import { db } from "./firebase.js";
-import { collection, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const q = query(collection(db, "players"), orderBy("score", "desc"));
+window.enableAutoAnimate();
+
+const q = query(
+  collection(db, "players"),
+  orderBy("score", "desc")
+);
 
 onSnapshot(q, (snap) => {
+
+  const players = [];
+
+  snap.forEach(doc => {
+    players.push(doc.data());
+  });
+
+  renderPodium(players);
+  renderTable(players);
+});
+
+function renderPodium(players) {
+
+  const podium = document.getElementById("podium");
+
+  const first = players[0];
+  const second = players[1];
+  const third = players[2];
+
+  podium.innerHTML = `
+    <div class="podium-place second">
+      <div class="place-number">🥈</div>
+      <div class="place-name">${second?.name || "-"}</div>
+      <div class="place-score">${second?.score || 0} баллов</div>
+    </div>
+
+    <div class="podium-place first">
+      <div class="place-number">🥇</div>
+      <div class="place-name">${first?.name || "-"}</div>
+      <div class="place-score">${first?.score || 0} баллов</div>
+    </div>
+
+    <div class="podium-place third">
+      <div class="place-number">🥉</div>
+      <div class="place-name">${third?.name || "-"}</div>
+      <div class="place-score">${third?.score || 0} баллов</div>
+    </div>
+  `;
+}
+
+function renderTable(players) {
+
   const table = document.getElementById("table");
 
   let html = `
     <tr>
-      <th>#</th>
+      <th>Место</th>
       <th>Имя</th>
       <th>Баллы</th>
     </tr>
   `;
 
-  let i = 1;
-
-  snap.forEach(doc => {
-    const d = doc.data();
+  players.slice(3, 10).forEach((p, index) => {
 
     html += `
       <tr>
-        <td>${i}</td>
-        <td>${d.name}</td>
-        <td>${d.score}</td>
+        <td>${index + 4}</td>
+        <td>${p.name}</td>
+        <td>${p.score}</td>
       </tr>
     `;
-
-    i++;
   });
 
   table.innerHTML = html;
-});
+}
